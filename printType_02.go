9eca131050c75d296ab211bf7230253285da82ba
Carlos <carlos_reyes1@mail.fresnostate.edu>
2016-02-01 09:20:41 -0800

    HW2

---
+++ printType_02.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var num = 24
+	var word = "Bazzinga"
+	var what = false
+
+	fmt.Printf("num is a %T \n", num)
+	fmt.Printf("word is a %T \n", word)
+	fmt.Printf("what is a %T \n", what)
+}
